3feb1ca9854abd193fa511ce354decfd1fe6ca54
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2026-03-13 17:06:21 +0700

    3 : Cho phép bên PrjEcormerceT8 lấy file từ file-manager

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -1,9 +1,11 @@
 
 import { Router } from "express";
 import fileManagerRoutes from "./file-manager.route";
+import mediaRoutes from "./media.route";
 
 const router = Router();
 
 router.use('/file-manager', fileManagerRoutes);
+router.use('/media', mediaRoutes);
 
 export default router;
---
+++ routes/media.route.ts
@@ -0,0 +1,9 @@
+
+import { Router } from "express";
+import * as mediaController from "../controllers/media.controller";
+
+const router = Router();
+
+router.get('/:filename', mediaController.getFile);
+
+export default router;
